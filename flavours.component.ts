4809860281f1b45fc398ebe8b4033756df10aa23
Rani2909 <144281123+Rani2909@users.noreply.github.com>
2023-10-26 19:07:26 -0600

    Add files via upload

---
+++ flavours.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flavours',
+  templateUrl: './flavours.component.html',
+  styleUrls: ['./flavours.component.css']
+})
+export class FlavoursComponent {
+public flavoursArray =[
+  {
+    id:"01",
+    fName:"Vanilla",
+    desc:"Lorem ipsum.....",
+    additionalRequest: "",
+    size:"",
+    displayPic:"vanilla.jpg"
+  },
+  {
+    id:"02",
+    fName:"Strawberry",
+    desc:"Lorem ipsum.....",
+    additionalRequest: "",
+    size:"",
+    displayPic:"strawberry.jpg"
+  },
+  {
+    id:"03",
+    fName:"Chocolate",
+    desc:"Lorem ipsum.....",
+    additionalRequest: "",
+    size:"",
+    displayPic:"chocolate.jpg"
+  },
+  {
+    id:"04",
+    fName:"Pista",
+    desc:"Lorem ipsum.....",
+    additionalRequest: "",
+    size:"",
+    displayPic:"pista.jpg"
+  },
+  {
+    id:"05",
+    fName:"Butter Pecan",
+    desc:"Lorem ipsum.....",
+    additionalRequest: "",
+    size:"",
+    displayPic:"Butter Pecan.jpeg"
+  },
+  {
+    id:"06",
+    fName:"Neapolitan",
+    desc:"Lorem ipsum.....",
+    additionalRequest: "",
+    size:"",
+    displayPic:"neapolitan.jpg"
+  },
+
+  {
+    id:"07",
+    fName:"Butterscotch",
+    desc:"Lorem ipsum.....",
+    additionalRequest: "",
+    size:"",
+    displayPic:"butterscotch.jpg"
+  },
+
+]
+}
