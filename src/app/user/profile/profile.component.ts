4b03bc5332ceddfacf8f49cfee098d7b4afd5a1d
sorawat <s.tanthikun802@mybvc.ca>
2023-12-08 02:50:18 -0700

    refactor: format

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -11,32 +11,32 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class ProfileComponent implements OnInit {
 
   customerData: any = {
-  first_name:  '',
-  last_name:   '',
-  email:   '',
-  phone:   '',
-  password:   '',
-  confirm_password:   '',
+    first_name: '',
+    last_name: '',
+    email: '',
+    phone: '',
+    password: '',
+    confirm_password: '',
   };
-  
 
-  constructor(private customerService: CustomerService, private dataService:CommonserviceService, private httpClient: HttpClient) {}
- public isEditing:boolean = false;
+
+  constructor(private customerService: CustomerService, private dataService: CommonserviceService, private httpClient: HttpClient) { }
+  public isEditing: boolean = false;
   ngOnInit() {
     const details = this.dataService.getStoredCustomerDetails() || {};
-    this.customerData.first_name =details[0].first_name ;
-    this.customerData.last_name =details[0].last_name ;
-    this.customerData.email =details[0].email ;
-    this.customerData.phone =details[0].phone ;
-    this.customerData.password =details[0].password ;
-    this.customerData.confirm_password =details[0].confirm_password ;
+    this.customerData.first_name = details[0].first_name;
+    this.customerData.last_name = details[0].last_name;
+    this.customerData.email = details[0].email;
+    this.customerData.phone = details[0].phone;
+    this.customerData.password = details[0].password;
+    this.customerData.confirm_password = details[0].confirm_password;
 
 
     console.log(this.customerData)
   }
 
   updateProfile() {
-    
+
     // this.customerService.setCustomerData(this.customerData);
   }
 }
