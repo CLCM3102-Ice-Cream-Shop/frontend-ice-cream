1fa6234be90623530600b5b452053f6503c2a95c
sorawat <s.tanthikun802@mybvc.ca>
2023-12-09 11:22:48 -0700

    feat: update menu

--- scripts/data.sql
+++ scripts/data.sql
@@ -1,10 +1,22 @@
 INSERT INTO product.menus (menu_id,name,description,display_pic,price) VALUES
-	 ('01','Vanillaaaaaaa','Lorem ipsum.....','vanilla.jpg',3.99),
-	 ('02','Strawberry','Lorem ipsum.....','strawberry.jpg',4.48),
-	 ('03','Chocolate','Lorem ipsum.....','chocolate.jpg',5.99),
-	 ('04','Pista','Lorem ipsum.....','pista.jpg',4.99),
-	 ('05','Butter Pecan','Lorem ipsum.....','Butter Pecan.jpeg',6.59),
-	 ('06','Neapolitan','Lorem ipsum.....','neapolitan.jpg',3.99),
-	 ('07','Butterscotch','Lorem ipsum.....','butterscotch.jpg',6.99),
-	 ('08','Raspberry Ripple','Lorem ipsum.....','Raspberry.jpg',7.99),
-	 ('09','test','Lorem ipsum.....','test',7.99);
+	 ('DRK01','Chocolate Milkshake','Lorem ipsum.....','chocolatems.jpg',5.99),
+	 ('DRK02','Strawberry Milkshake','Lorem ipsum.....','strawberryms.jpg',3.99),
+	 ('DRK03','Vanilla Milkshake','Lorem ipsum.....','Vanillams.jpg',4.99),
+	 ('DRK04','Sunset Slushie','Lorem ipsum.....','SunsetSlushie.jpg',2.99),
+	 ('DRK05','Red Wine Ice Cream Float','Lorem ipsum.....','redwineicecreamfloat.jpg',4.99),
+	 ('DRK06','Pineapple Slushie','Lorem ipsum.....','pineappleslushie.jpg',1.99),
+	 ('DRK07','Root Beer Float','Lorem ipsum.....','RootBeerFloat.jpg',6.99),
+	 ('DRK08','Butterscotch Milkshake','Lorem ipsum.....','butterscotchmilkshake.jpg',2.99),
+	 ('FLV01','Vanillaaaaaaa','Lorem ipsum.....','vanilla.jpg',3.99),
+	 ('FLV02','Strawberry','Lorem ipsum.....','strawberry.jpg',4.48);
+INSERT INTO product.menus (menu_id,name,description,display_pic,price) VALUES
+	 ('FLV03','Chocolate','Lorem ipsum.....','chocolate.jpg',5.99),
+	 ('FLV04','Pista','Lorem ipsum.....','pista.jpg',4.99),
+	 ('FLV05','Butter Pecan','Lorem ipsum.....','Butter Pecan.jpeg',6.59),
+	 ('FLV06','Neapolitan','Lorem ipsum.....','neapolitan.jpg',3.99),
+	 ('FLV07','Butterscotch','Lorem ipsum.....','butterscotch.jpg',6.99),
+	 ('FLV08','Raspberry Ripple','Lorem ipsum.....','Raspberry.jpg',7.99),
+	 ('SPC01','Frozen caramel nut crunch ice cream','Indulge in the exquisite delight of Frozen Caramel Nut Crunch Ice Cream, a symphony of rich flavors and satisfying textures. Creamy caramel-infused ice cream is generously studded with crunchy nut clusters, creating a harmonious blend of sweet and savory. Each spoonful is a journey through velvety smoothness and delightful crunch, making this frozen treat a perfect balance of indulgence and refreshment. Elevate your dessert experience with the luscious taste of caramel and the irresistible allure of nutty goodness in every bite.','Frozen_caramel_nut_crunch_ice_cream_pie.png',11.99),
+	 ('SPC02','Strawberry Lemon Shortcake Sundae','Savor the blissful combination of summer sweetness with our Strawberry Lemon Shortcake Sundae. Immerse yourself in layers of ripe, succulent strawberries and zesty lemon-infused shortcake, crowned with velvety vanilla ice cream. The medley of flavors unfolds with each spoonful, as the tartness of fresh strawberries dances with the citrusy notes of lemon, harmonizing perfectly with the buttery, crumbly shortcake. This sundae is a delightful symphony of textures and tastes, capturing the essence of sunny days and the joy of indulging in a classic dessert with a fruity twist.','Strawberry-Lemon-Shortcake-Sundae.png',10.99),
+	 ('SPC03','Vanilla Pudding Cookie Trifle','Experience a heavenly delight with our Vanilla Pudding Cookie Trifle. Layers of velvety vanilla pudding intertwine with delicate, crumbly cookies, creating a symphony of textures and flavors. Each spoonful unveils the perfect balance between the creamy richness of the pudding and the satisfying crunch of the cookies. The vanilla undertones provide a classic sweetness, while the interplay of textures makes for a dessert experience that is both comforting and indulgent. Elevate your dessert cravings with this decadent trifle, where simplicity meets sophistication in a delightful culinary fusion.','Vanilla-Pudding-Cookie-Trifle.png',13.99),
+	 ('SPC04','Mile High Birthday Ice Cream Cake','Celebrate in grand style with our Mile High Birthday Ice Cream Cake, a towering masterpiece of pure delight. Layers of premium ice cream, each bursting with vibrant flavors, are meticulously stacked to create a towering confection. Rich chocolate and velvety vanilla harmonize with swirls of indulgent caramel, while chunks of colorful birthday cake add festive flair. Topped with a luscious frosting and a cascade of rainbow sprinkles, this decadent creation promises a celebration like no other. Every slice is a journey through layers of joy, making this cake the perfect centerpiece for a birthday extravaganza.','Mile-High-Birthday-Ice-Cream-Cake.png',15.99);
