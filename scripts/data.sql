3cd3e383f1e392d9e6b4e954bbc0f5243177c1b4
sorawat <s.tanthikun802@mybvc.ca>
2023-12-09 00:42:57 -0700

    build: add sql file

---
+++ scripts/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO product.menus (menu_id,name,description,display_pic,price) VALUES
+	 ('01','Vanillaaaaaaa','Lorem ipsum.....','vanilla.jpg',3.99),
+	 ('02','Strawberry','Lorem ipsum.....','strawberry.jpg',4.48),
+	 ('03','Chocolate','Lorem ipsum.....','chocolate.jpg',5.99),
+	 ('04','Pista','Lorem ipsum.....','pista.jpg',4.99),
+	 ('05','Butter Pecan','Lorem ipsum.....','Butter Pecan.jpeg',6.59),
+	 ('06','Neapolitan','Lorem ipsum.....','neapolitan.jpg',3.99),
+	 ('07','Butterscotch','Lorem ipsum.....','butterscotch.jpg',6.99),
+	 ('08','Raspberry Ripple','Lorem ipsum.....','Raspberry.jpg',7.99),
+	 ('09','test','Lorem ipsum.....','test',7.99);
---
+++ scripts/schema.sql
@@ -0,0 +1,74 @@
+CREATE DATABASE `customer`;
+CREATE DATABASE `payment`; 
+CREATE DATABASE `staff`;
+CREATE DATABASE `product`;
+
+CREATE TABLE customer.`customer` (
+  `customer_id` varchar(100) NOT NULL,
+  `first_name` varchar(256) NOT NULL,
+  `last_name` varchar(256) NOT NULL,
+  `email_address` varchar(256) DEFAULT NULL,
+  `phone_number` varchar(100) DEFAULT NULL,
+  `password_hash` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE KEY `customer_un` (`email_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE staff.`staff` (
+  `staff_id` varchar(100) NOT NULL,
+  `first_name` varchar(256) NOT NULL,
+  `last_name` varchar(256) NOT NULL,
+  `email_address` varchar(256) DEFAULT NULL,
+  `phone_number` varchar(100) DEFAULT NULL,
+  `password_hash` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`staff_id`),
+  UNIQUE KEY `customer_un` (`email_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE payment.`cart`(
+  `cart_id` varchar(100) NOT NULL,
+  `no` int(11) NOT NULL,
+  `customer_id` varchar(100) NOT NULL,
+  `date` varchar(10) DEFAULT NULL,
+  `menu_id` varchar(100) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  `properties` varchar(255) DEFAULT NULL,
+  `additional_request` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE payment.`discounts` (
+  `discount_code` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `value` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`discount_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- payment.orders definition
+
+CREATE TABLE payment.`orders` (
+  `order_id` varchar(100) NOT NULL,
+  `cart_id` varchar(100) NOT NULL,
+  `customer_id` varchar(100) NOT NULL,
+  `sub_total` decimal(10,2) DEFAULT NULL,
+  `discount_code` varchar(100) NOT NULL,
+  `total_amount` decimal(10,2) DEFAULT NULL,
+  `date_time` datetime DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- product.menus definition
+
+CREATE TABLE product.`menus` (
+  `menu_id` varchar(10) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `display_pic` varchar(100) DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
