84a73169a385a25b79d49c5453636ecfdae1e765
sorawat <s.tanthikun802@mybvc.ca>
2023-12-08 22:31:17 -0700

    feat: add all url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
     apiCustomerUrl: 'http://54.234.168.231:5000',
-    apiPaymentUrl: 'http://54.234.168.231:3101'
+    apiPaymentUrl: 'http://54.234.168.231:3101',
+    apiProductUrl: 'http://54.234.168.231:3102',
+    apiReportUrl: 'http://54.234.168.231:3103'
   };
