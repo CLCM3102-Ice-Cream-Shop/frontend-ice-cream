0b8dbc7bfc28ed1cf02f574d1ee77909aa15b813
sorawat <s.tanthikun802@mybvc.ca>
2023-12-09 14:15:37 -0700

    feat: modify get list of order

--- src/app/admin/admin-order-details/admin-order-details.component.ts
+++ src/app/admin/admin-order-details/admin-order-details.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonserviceService } from '../../commonservice.service';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-order-details',
@@ -9,22 +11,49 @@ import { CommonserviceService } from '../../commonservice.service';
 export class AdminOrderDetailsComponent {
   public cartItems: any[] = [];
 
-  constructor(private commonservice: CommonserviceService) { }
+  constructor(private commonservice: CommonserviceService, private httpClient: HttpClient,) { }
 
   ngOnInit(): void {
+    const apiUrl = `${environment.apiPaymentUrl}/order/active`;
+    this.httpClient.get<any>(apiUrl).subscribe(
+      (response) => {
+        if (response && response.data) {
+          const mappedData = response.data.map((item: any) => {
+            return {
+              orderId: item.order_id,
+              customerId: item.customer_id,
+              cartId: item.cart_id,
+              cartDetail: item.cart_detail.map((detail: any) => ({
+                cartId: detail.cart_id,
+                no: detail.no,
+                date: detail.date,
+                menuId: detail.menu_id,
+                menuName: detail.menu_name,
+                price: detail.price,
+                quantity: detail.quantity,
+                status: detail.status,
+                properties: detail.properties,
+                additionalRequest: detail.additional_request
+              })),
+              subTotal: parseFloat(item.sub_total),
+              totalAmount: parseFloat(item.total_amount),
+              dateTime: new Date(item.date_time),
+              status: item.status
+            };
+          });
     
-    this.cartItems = this.commonservice.getCartItems();
-    this.cartItems = this.cartItems.map((item, index) => {
-      const productType = item.productType || 'default'; 
-      const orderid = `${productType.toUpperCase()}${('000' + (index + 1)).slice(-3)}`;
-      return {
-        ...item,
-        orderid: orderid
-      
-    }
-  });
-    console.log(this.cartItems, "at admin order details page");
+          this.cartItems = mappedData;
+        }
+      },
+      (error) => {
+      }
+    );
+
+    // this.cartItems = this.commonservice.getCartItems();
+    // }
+    // });
+    //   console.log(this.cartItems, "at admin order details page");
+    // }
+
   }
-
-
 }
