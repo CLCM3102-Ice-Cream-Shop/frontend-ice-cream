db4cdbab49ef2401aa3b337c03d555f2c2c9a80e
sorawat <s.tanthikun802@mybvc.ca>
2023-12-11 01:06:52 -0700

    build: add `docker` environment for docker compose

---
+++ src/environments/environment.docker.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiCustomerUrl: 'http://backend-customer-service:5000',
+    apiPaymentUrl: 'http://backend-payment-service:3101',
+    apiProductUrl: 'http://backend-product-service:3102',
+    apiReportUrl: 'http://backend-report-service:3103'
+  };
