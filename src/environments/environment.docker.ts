f720ee8e2fa734e333218b57f20b1c8e3999ab61
sorawat <s.tanthikun802@mybvc.ca>
2023-12-11 01:41:26 -0700

    fix: change to localhost

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiCustomerUrl: 'http://backend-customer-service:5000',
-    apiPaymentUrl: 'http://backend-payment-service:3101',
-    apiProductUrl: 'http://backend-product-service:3102',
-    apiReportUrl: 'http://backend-report-service:3103'
+    apiCustomerUrl: 'http://localhost:5000',
+    apiPaymentUrl: 'http://localhost:3101',
+    apiProductUrl: 'http://localhost:3102',
+    apiReportUrl: 'http://localhost:3103'
   };
